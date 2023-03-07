d794fac5e1c1284adf276a78a8022e45bb1fd7d7
gasangw <thomasgasangwa@gmail.com>
2023-03-07 20:03:08 +0200

    Add filters for both regions and countries

--- src/components/Nav/Navigation.jsx
+++ src/components/Nav/Navigation.jsx
@@ -2,10 +2,13 @@ import { BsMoon } from "react-icons/bs";
 import './nav.css'
 
 const Navigation =() => {
+  const changeMode =()=> {
+    ////Add styles for the dark mode
+  }
   return (
       <div className='nav-container'>
         <p className="nav-para">Where in the world?</p>
-          <span className="nav-icon"> <BsMoon /> Dark Mode</span>
+          <span className="nav-icon" onClick={changeMode}> <BsMoon /> Dark Mode</span>
       </div>
   );
 }
